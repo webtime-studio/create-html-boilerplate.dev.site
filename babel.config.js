966b3347d906a4ffaf9d78a9373892651a4503fb
Pavel Klochkov <paul.klochkov@gmail.com>
2020-09-20 15:41:34 +0200

    Починил бабель

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,12 @@
 module.exports = {
   presets: [require.resolve("@docusaurus/core/lib/babel/preset")],
   plugins: [
-    require.resolve("babel-plugin-styled-components"),
-    {
-      ssr: true,
-      displayName: true,
-      fileName: true,
-    },
+    [
+      "babel-plugin-styled-components",
+      {
+        displayName: true,
+        fileName: true,
+      },
+    ],
   ],
 };
