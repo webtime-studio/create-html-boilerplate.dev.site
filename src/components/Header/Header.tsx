bfe3627cdd0e5096477b2f4cf6006d3b97340edf
Nefila <veronika.sts@gmail.com>
2020-09-23 11:05:25 +0200

    Добавляет компонент Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,27 +3,24 @@ import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import useBaseUrl from "@docusaurus/useBaseUrl";
 import clsx from "clsx";
-import styles from "./header.module.scss";
+import {Buttons} from "../../components";
+import "./header.scss";
 
-const Header = () => {
+interface HeaderProps {
+  children: React.ReactNode | null;
+}
+
+const Header: React.FC<HeaderProps> = ({children: buttons}) => {
   const context = useDocusaurusContext();
   const { siteConfig = {} } = context;
 
   return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.button}>
-          <Link
-            className={clsx(
-              "button button--outline button--secondary button--lg",
-              styles.getStarted
-            )}
-            to={useBaseUrl("docs/")}
-          >
-            Get Started
-          </Link>
+    <header className="header">
+      <div className="header__content-wrapper">
+        <h1 className="header__title">{siteConfig.title}</h1>
+        <p className="header__slogan">{siteConfig.tagline}</p>
+        <div className="header__buttons">
+          {buttons}
         </div>
       </div>
     </header>
