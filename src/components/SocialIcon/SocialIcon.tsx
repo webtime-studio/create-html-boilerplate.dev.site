bebf54fd70c5ff3af144cac97914b792a8059344
Nefila <veronika.sts@gmail.com>
2020-09-28 14:18:37 +0200

    Удаляет console.log

--- src/components/SocialIcon/SocialIcon.tsx
+++ src/components/SocialIcon/SocialIcon.tsx
@@ -8,16 +8,13 @@ interface SocialIconType {
   iconType: string;
 };
 
-const SocialIcon: React.FC<SocialIconType> = ({ iconType }) => {
-  console.log(iconType);
-  return (
-    <>
-      { iconType === 'github' ? <GitHubIcon /> : null}
-      { iconType === 'linkedin' ? <LinkedinIcon /> : null}
-      { iconType === 'facebook' ? <FacebookIcon /> : null}
-      { iconType === 'youtube' ? <YouTubeIcon /> : null}
-    </>
-  )
-};
+const SocialIcon: React.FC<SocialIconType> = ({ iconType }) => (
+  <>
+    { iconType === 'github' ? <GitHubIcon /> : null}
+    { iconType === 'linkedin' ? <LinkedinIcon /> : null}
+    { iconType === 'facebook' ? <FacebookIcon /> : null}
+    { iconType === 'youtube' ? <YouTubeIcon /> : null}
+  </>
+);
 
 export default SocialIcon;
