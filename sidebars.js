10002bbf93773d02fdeae84b9c738eff36e15a58
Pavel Klochkov <paul.klochkov@gmail.com>
2020-10-03 13:26:04 +0200

    Обновляем пару зависимостей Докузаураса и одну страницу документации

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   someSidebar: {
-    Docusaurus: ['doc1', 'doc2', 'doc3'],
-    Features: ['mdx'],
+    Docusaurus: ["how-to-start", "doc3", "doc1"],
+    Features: ["mdx"],
   },
 };
