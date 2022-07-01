44b2614908e963436642d2ec39cd60a4fcdfaa21
Pavel Klochkov <paul.klochkov@gmail.com>
2022-07-02 00:05:52 +0200

    Update deps, fix small features, add new CHB release info

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -96,13 +96,13 @@ module.exports = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           editUrl:
-            "https://github.com/facebook/docusaurus/edit/master/website/",
+            "https://github.com/webtime-studio/create-html-boilerplate.dev.site/blob/master/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
-          editUrl:
-            "https://github.com/facebook/docusaurus/edit/master/website/blog/",
+          // editUrl:
+          //   "https://github.com/facebook/docusaurus/edit/master/website/blog/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
